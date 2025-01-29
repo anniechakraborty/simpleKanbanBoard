20324a0172d91b7d20be166f302628fdf6895492
anniechakraborty <the.demented.genius29@gmail.com>
2025-01-29 19:19:16 +0100

    fixed the routing issue and installed bootstrap

--- src/app/components/menu/menu.component.ts
+++ src/app/components/menu/menu.component.ts
@@ -1,8 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-menu',
-  imports: [],
+  imports: [
+    RouterLink,
+    RouterLinkActive
+  ],
   templateUrl: './menu.component.html',
   styleUrl: './menu.component.scss'
 })
