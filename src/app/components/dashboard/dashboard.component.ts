57ab11915042bdb225bfabb352dd0a3fd8f35823
anniechakraborty <the.demented.genius29@gmail.com>
2025-02-02 00:58:36 +0100

    create and get all tasks done

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,25 +1,68 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+
+import { TasksInterface } from '../../interfaces/tasks-interface';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, CommonModule],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  private apiUrl: string = "http://127.0.0.1:5000/tasks";
+  protected todoTasks: [] = [];
+  protected inProgressTasks: [] = [];
+  protected finishedTasks: [] = [];
+
   tasksForm = new FormGroup({
     title: new FormControl('', Validators.required),
     description: new FormControl('', Validators.required),
     status: new FormControl(''),
-  })
+  });
 
-  // addTask(): void {
-  //   console.log('add task');
-  // }
+  constructor(
+    private http: HttpClient
+  ){}
+
+  ngOnInit(): void {
+      this.getAllTasks()
+  }
 
   onSubmit(): void{
     console.log(this.tasksForm.value);
-    // Make API call to authenticate user
+    this.http.post(
+      this.apiUrl + '/',
+      this.tasksForm.value,
+      {
+        headers: {
+          'Authorization': 'Bearer ' + localStorage.getItem('token')
+        }
+      }
+    ).subscribe(data => {
+      console.log('Task added successfully', data);
+      this.tasksForm.reset();
+      this.getAllTasks();
+    })
+  }
+
+  getAllTasks() {
+    this.http.get(
+      this.apiUrl + '/',
+      {
+        headers: {
+          'Authorization': 'Bearer '+ localStorage.getItem('token')
+        }
+      }
+    ).subscribe((res: any) => {
+      console.log('Tasks fetched : ', res);
+      if(res && res.task){
+        this.todoTasks = res.task.filter((task: TasksInterface)=> Number(task.status) === 0);
+        this.inProgressTasks = res.task.filter((task: TasksInterface)=> Number(task.status) === 1);
+        this.finishedTasks = res.task.filter((task: TasksInterface)=> Number(task.status) === 2);
+      }
+    })
   }
 }
---
+++ src/app/interfaces/tasks-interface.ts
@@ -0,0 +1,8 @@
+export interface TasksInterface {
+    _id: string;
+    title: string;
+    description: string;
+    status: string;
+    created_on: string;
+    created_by: string;
+}
