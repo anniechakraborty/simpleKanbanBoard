d9cc15f7cc42bfc9bedfbbf2a163d2a8db0fe30a
anniechakraborty <the.demented.genius29@gmail.com>
2025-02-01 01:43:49 +0100

    getting user from active token and keeping user logged in

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { MenuComponent } from './components/menu/menu.component';
+import { HttpClient } from '@angular/common/http';
+import { AuthServiceService } from './service/auth-service.service';
 
 @Component({
   selector: 'app-root',
@@ -17,6 +19,26 @@ import { MenuComponent } from './components/menu/menu.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'simpleKanbanBoard';
+  private apiUrl: string = 'http://127.0.0.1:5000/auth';
+  constructor (
+    private http: HttpClient,
+    private authService: AuthServiceService,
+  ){}
+  // Get current user to verify a user is logged or not
+  ngOnInit(): void {
+      this.http.get(
+        this.apiUrl + '/user', 
+        {
+          headers: {
+            'Authorization': 'Bearer ' + localStorage.getItem('token')
+          }
+        }
+      ).subscribe((res: any) => {
+        console.log('User response : ', res);
+        // If user is authenticated, set the user data in the service
+        this.authService.currentUserSignal.set(res);
+      })
+  }
 }
--- src/app/service/auth-service.service.ts
+++ src/app/service/auth-service.service.ts
@@ -7,11 +7,19 @@ import { UserInterface } from '../interfaces/user-interface';
 export class AuthServiceService {
 
   constructor() { }
-  // Signal allows us to access the values inside it from different points in the application
-  // Signal will notify all components to render
+  /**
+   * Signal allows us to access the values inside it from different points in the application. 
+   * It will notify all components to render the changes in the value.
+   * 
+   * Everytime we reload the application, signal becomes undefined. Or in other words, our user will get logged out. 
+   * Hence, we need to relogin using the token saved in local storage everytime the application is loaded.
+   * 
+   * States:
+   * - undefined : initial state
+   * - null: unauthorized
+   * - UserInterface : type of data that this signal will hold when the user is logged in
+   */
+  // 
   currentUserSignal = signal<UserInterface | undefined | null>(undefined);
-  // undefined - initial state
-  // null - unauthorized
-  // UserInterface - type of data that this signal will hold when the user is logged in
   
 }
